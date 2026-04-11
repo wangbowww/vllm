b175e2e4c4af122f0d0ebedb4bba24b57edd285e
wangbowww <weirdowww@qq.com>
2026-04-11 12:27:50 +0800

    Update basic offline inference example

--- examples/basic/offline_inference/basic.py
+++ examples/basic/offline_inference/basic.py
@@ -16,7 +16,7 @@ sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
 def main():
     # Create an LLM.
-    llm = LLM(model="facebook/opt-125m")
+    llm = LLM(model="/data1/models/Qwen3-0.6B", gpu_memory_utilization = 0.3)
     # Generate texts from the prompts.
     # The output is a list of RequestOutput objects
     # that contain the prompt, generated text, and other information.
@@ -32,4 +32,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main() 
